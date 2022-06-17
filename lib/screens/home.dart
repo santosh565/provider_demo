dbb29c800225fd4edb41965438a2cf25cbc473aa
Santosh Bhattarai <hsotnas76@gmail.com>
2022-06-17 14:12:11 +0545

    update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -15,27 +15,28 @@ class Home extends StatelessWidget {
         title: const Text('Wax app'),
         actions: [
           IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const Settings(),
-                  ),
-                );
-              },
-              icon: const Icon(Icons.settings))
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const Settings(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.settings),
+          )
         ],
       ),
       body: ListView.builder(
-          itemCount: reports.length,
-          itemBuilder: (context, index) {
-            final report = reports[index];
-            return ListTile(
-              title: Text(report.line),
-              subtitle: Text(report.wax),
-            );
-          },
-          ),
+        itemCount: reports.length,
+        itemBuilder: (context, index) {
+          final report = reports[index];
+          return ListTile(
+            title: Text(report.line),
+            subtitle: Text(report.wax),
+          );
+        },
+      ),
     );
   }
 }
